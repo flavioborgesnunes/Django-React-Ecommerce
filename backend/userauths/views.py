21639680a609318c2a42e1957d2a79af3488e72d
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-01-30 13:15:38 -0300

    feat: Init userauths/ views.py

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from userauths.models import Profile, User
+from userauths.serializer import (MyTokenObtainPairSerializer,
+                                  RegisterSerializer)
 
-# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
