9adc77f193e58498044f9de0ee0371a118a7346c
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-01-31 09:49:29 -0300

    feat: create RegisterView

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from userauths.models import Profile, User
 from userauths.serializer import (MyTokenObtainPairSerializer,
@@ -7,3 +9,9 @@ from userauths.serializer import (MyTokenObtainPairSerializer,
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegisterSerializer
