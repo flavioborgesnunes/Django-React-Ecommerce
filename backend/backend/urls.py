8d0fc2d38ad9d9a6a4a82b4b11a592956e24ca43
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-01-31 10:26:12 -0300

    feat create and installing yasg

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,10 +18,36 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce Bakend API",
+        default_version='v1',
+        description="This is the documentation  for backend api",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="flavioborgesnunes@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+
+    # Documentation
+    path('', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+
+    # DOCUMENTATION FROM YASG DOCS
+    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
+    #      name='schema-json'),
+    # path('redoc/', schema_view.with_ui('redoc',
+    #      cache_timeout=0), name='schema-redoc'),
 
 ]
 
