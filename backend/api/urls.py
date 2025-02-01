67d79e1add22f92d6c9d0a461e4f932ad2b99305
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-01 10:32:26 -0300

    feat: incert refresh in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from store import views as store_views
 from userauths import views as userauths_views
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view()),
 ]
