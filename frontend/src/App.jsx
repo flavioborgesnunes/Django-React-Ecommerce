9c4dbbe5569f32fd48f15a59b02c9e0ad297b2e5
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-05 12:22:50 -0300

    feat: create register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './views/auth/Login'
+import Register from './views/auth/Register'
+import Dashboard from './views/auth/Dashboard'
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
 
