89a6c24c67abde50a34e00a60d5562743b234a02
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-07 12:49:12 -0300

    feat: create backend reset password

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,10 +1,13 @@
+import random
+
+import shortuuid
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from userauths.models import Profile, User
 from userauths.serializer import (MyTokenObtainPairSerializer,
-                                  RegisterSerializer)
+                                  RegisterSerializer, UserSerializer)
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -15,3 +18,28 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny, )
     serializer_class = RegisterSerializer
+
+
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
+
+
+class PasswordResetEmailVerify(generics.RetrieveAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+        if user:
+            user.otp = generate_otp()
+            user.save()
+
+            uidb64 = user.pk
+            otp = user.otp
+            link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+            print(link)
+        return user
