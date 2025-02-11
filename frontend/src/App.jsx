2ee96fac8b8db8b711181c98433814f759c8256f
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-11 11:01:16 -0300

    feat: create password frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,27 @@ import Dashboard from './views/auth/Dashboard'
 import Logout from './views/auth/Logout'
 import ForgotPassword from './views/auth/ForgotPassword'
 import CreatePassword from './views/auth/CreatePassword'
-
+import StoreHeader from './views/base/StoreHeader'
+import Storefooter from './views/base/StoreFooter'
+import MainWrapper from './layout/mainWrapper'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/create-new-password" element={<CreatePassword />} />
-      </Routes>
+      <StoreHeader />
+      <MainWrapper>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-new-password" element={<CreatePassword />} />
+        </Routes>
+      </MainWrapper>
+      <Storefooter />
     </BrowserRouter>
 
   )
