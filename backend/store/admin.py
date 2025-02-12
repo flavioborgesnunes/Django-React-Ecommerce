bff51d715115f9d00f9fa1448965fbd6f6c3fbbf
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-12 10:38:50 -0300

    feat: register galery, specifications, size and color models  in admin.py

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from store.models import Category, Product
+from store.models import Category, Color, Galery, Product, Size, Specification
 
-admin.site.register(Product)
+
+class GaleryInline(admin.TabularInline):
+    model = Galery
+    extra = 0
+
+
+class SpecificationInline(admin.TabularInline):
+    model = Specification
+    extra = 0
+
+
+class SizeInline(admin.TabularInline):
+    model = Size
+    extra = 0
+
+
+class ColorInline(admin.TabularInline):
+    model = Color
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "category", "shipping_amount",
+                    "stock_qty", "in_stock", "vendor", "featured"]
+    list_editable = ["featured"]
+    list_filter = ["date"]
+    inlines = [GaleryInline, SpecificationInline,
+               SizeInline, ColorInline]
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
