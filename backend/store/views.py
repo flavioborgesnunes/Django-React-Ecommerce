b9b7277ee102c104459d43987321193652d26a2e
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-16 10:52:04 -0300

    feat: create cart api view(init)

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from store.models import Category, Product
-from store.serializers import CategorySerializer, ProductSerializer
+from store.models import (Cart, CartOrder, CartOrderItem, Category, Color,
+                          Coupon, Gallery, Notification, Product, ProductFaq,
+                          Review, Size, Specification, Wishlist)
+from store.serializers import (CartOrderItemSerializer, CartOrderSerializer,
+                               CartSerializer, CategorySerializer,
+                               ProductSerializer)
 
 
 class CategoryListAPIView(generics.ListAPIView):
@@ -24,3 +28,9 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     def get_object(self):
         slug = self.kwargs['slug']
         return Product.objects.get(slug=slug)
+
+
+class CartAPIView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_class = [AllowAny]
