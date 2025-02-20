610cd974e7ae85229177e83f238678acf6cdc158
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-02-20 16:16:41 -0300

    fix: update foreign key from CASCADE to SET_NULL

--- backend/store/models.py
+++ backend/store/models.py
@@ -143,7 +143,8 @@ class Color(models.Model):
 
 class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
     qty = models.PositiveIntegerField(default=0)
     price = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
     sub_total = models.DecimalField(
